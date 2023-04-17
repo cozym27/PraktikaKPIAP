3143071a0643d8991b771b71585221584e757f5f
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-17 13:27:22 +0300

    Задание 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -6,12 +6,28 @@ using System.Threading.Tasks;
 
 namespace _1
 {
-    internal class Program
+    using System;
+
+    class Program
     {
         static void Main(string[] args)
         {
-           // Console.writeline
-           // fttgtggygyhyh
+            double a, b, s, p;
+
+            Console.Write("Введите сторону a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите сторону b: ");
+            b = double.Parse(Console.ReadLine());
+
+            s = a * b;
+            p = 2 * (a + b);
+
+            Console.WriteLine("Площадь прямоугольника: {0}", s);
+            Console.WriteLine("Периметр прямоугольника: {0}", p);
+
+            Console.ReadKey();
         }
     }
+
 }
