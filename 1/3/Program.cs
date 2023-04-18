b5db68ec17c28ac03147ecf898be1168b7f832d9
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-18 09:31:32 +0300

    ЗАДАНИЕ 3

---
+++ 1/3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double m, ton;
+
+            Console.Write("Введите массу в килограммах: ");
+            m = double.Parse(Console.ReadLine());
+
+            ton = m / 1000;
+
+            Console.WriteLine("Количество полных тонн: {0}", (int)ton);
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
