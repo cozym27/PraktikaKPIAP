1880f22e1c504d9aa1c9edd2e8ffa87085535da5
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-18 09:14:18 +0300

    ЗАДАНИЕ 2

---
+++ 1/2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, temp;
+
+            Console.Write("Введите значение a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение b: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение c: ");
+            c = double.Parse(Console.ReadLine());
+
+            temp = a;
+            a = b;
+            b = c;
+            c = temp;
+
+            Console.WriteLine("Новое значение a: {0}", a);
+            Console.WriteLine("Новое значение b: {0}", b);
+            Console.WriteLine("Новое значение c: {0}", c);
+
+            Console.ReadKey();
+        }
+    }
+
+}
