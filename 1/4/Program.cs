48cf093764494b05cf7928bb1ec7c9cc5c98eade
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-18 10:15:04 +0300

    ЗАДАНИЕ 4

---
+++ 1/4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, minutes;
+
+            Console.Write("Введите количество секунд: ");
+            n = int.Parse(Console.ReadLine());
+
+            minutes = n / 60;
+
+            Console.WriteLine("Количество полных минут: {0}", minutes);
+
+            Console.ReadKey();
+        }
+    }
+
+}
