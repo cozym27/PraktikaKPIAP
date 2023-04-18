1c1c0bd55f74f8c35de1fcadc7c4dc48e4ce624d
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-18 11:01:51 +0300

    ЗАДАНИЕ 5

---
+++ 1/5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = -4.5;
+            double y = 0.75 * Math.Pow(10, -4);
+            double z = 0.845 * Math.Pow(10, 2);
+            double u = -55.6848;
+
+            double result = ((Math.Pow(8 + Math.Pow(Math.Abs(x - y), 2) + 1, 1/3)) / (Math.Pow(x, 2) + Math.Pow(y, 2) + 2)) - Math.Exp(Math.Abs(x - y)) * Math.Pow(Math.Tan(z), 2 * x + 1);
+
+            Console.WriteLine("Результат: " + result);
+            Console.WriteLine("Правильный ответ: " + u);
+            Console.ReadKey();
+        }
+    }
+
+}
