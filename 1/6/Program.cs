2befeb80a1285893deed24752c21f10ccd7ee803
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-18 12:33:30 +0300

    ЗАДАНИЕ 6

---
+++ 1/6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:"); // выводим сообщение пользователю
+            int num = Convert.ToInt32(Console.ReadLine()); // считываем число, введенное пользователем
+
+            if (num > 0)
+            {
+                num += 1; // прибавляем 1, если число положительное
+            }
+            else
+            {
+                num -= 2; // вычитаем 2, если число отрицательное или равно 0
+            }
+
+            Console.WriteLine($"Результат: {num}"); // выводим результат
+            Console.ReadKey(); // ожидаем нажатия клавиши для завершения программы
+        }
+    }
+
+
+}
