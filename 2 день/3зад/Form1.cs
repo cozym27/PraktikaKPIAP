32e38bf619576b22cbda074b84e504537df91564
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-19 10:09:27 +0300

    ЗАДАНИЕ 3

---
+++ 2 день/3зад/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3зад
+{
+    
+    
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void buttonCalculate_Click(object sender, EventArgs e)
+            {
+                double x, v, sum;
+                int n;
+                const double a = 0.001;
+
+                if (!double.TryParse(textBoxX.Text, out x))
+                {
+                    MessageBox.Show("Введите корректное значение x");
+                    return;
+                }
+
+                v = x;
+                sum = 0;
+                n = 1;
+
+                while (Math.Abs(v) > a)
+                {
+                    sum += v;
+                    n++;
+                    v *= -x * n / (n - 1);
+                }
+
+                textBoxResult.Text = sum.ToString();
+            }
+        }
+    
+
+}
