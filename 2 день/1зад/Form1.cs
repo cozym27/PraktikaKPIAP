c5dd10bce6e0a83c8fd31f877c0096710f5b2647
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-19 09:32:39 +0300

    ЗАДАНИЕ 1

---
+++ 2 день/1зад/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1зад
+{
+    public partial class Form1 : Form
+    {
+        private void button1_Click_1(object sender, EventArgs e)
+        double x = double.Parse(textBox1.Text);
+        double y = double.Parse(textBox2.Text);
+        double z = double.Parse(textBox3.Text);
+        double u = -55.6848;
+
+        double result = ((Math.Pow(8 + Math.Pow(Math.Abs(x - y), 2) + 1, 1 / 3)) / (Math.Pow(x, 2) + Math.Pow(y, 2) + 2)) - Math.Exp(Math.Abs(x - y)) * Math.Pow(Math.Tan(z), 2 * x + 1);
+
+        textBox4.Text = "Результат: " + result.ToString();
+        textBox5.Text = "Правильный ответ: " + u.ToString();
+            
+    }
+}
