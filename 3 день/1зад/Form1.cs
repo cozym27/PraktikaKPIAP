d7ca04e0913b490f8de17f73d2b2470552ae5f8a
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-19 10:19:21 +0300

    ЗАДАНИЕ 1

---
+++ 3 день/1зад/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1зад
+{
+   
+    
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void buttonCalculate_Click(object sender, EventArgs e)
+            {
+                int m, n;
+                if (!int.TryParse(textBoxM.Text, out m))
+                {
+                    MessageBox.Show("Введите корректное значение m");
+                    return;
+                }
+
+                if (!int.TryParse(textBoxN.Text, out n))
+                {
+                    MessageBox.Show("Введите корректное значение n");
+                    return;
+                }
+
+                int gcd = GCD(m, n);
+                int lcm = m * n / gcd;
+
+                textBoxResult.Text = lcm.ToString();
+            }
+
+            private int GCD(int a, int b)
+            {
+                while (b != 0)
+                {
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+                return a;
+            }
+        }
+    
+
+}
