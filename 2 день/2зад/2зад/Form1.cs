836047e891ef7c618f20455c8248cc323d861242
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-20 09:56:28 +0300

    ...

--- 2 день/2зад/2зад/Form1.cs
+++ 2 день/2зад/2зад/Form1.cs
@@ -10,28 +10,21 @@ using System.Windows.Forms;
 
 namespace _2зад
 {
-    
-
-    
-        public partial class Form1 : Form
+    public partial class Form1 : Form
+    {
+        public Form1()
         {
-            public Form1()
-            {
-                InitializeComponent();
-            }
-
-            private void button1_Click(object sender, EventArgs e)
-            {
-                double a, b, expResult, eValue;
-                double.TryParse(textBox1.Text, out a);
-                double.TryParse(textBox2.Text, out b);
-                double.TryParse(textBox3.Text, out eValue);
-
-                expResult = Math.Log((Math.Sin(Math.Pow(a, 2)) + Math.Cos(b)) / Math.Sqrt(1 + (eValue / (Math.Pow(a, 3) + 3.4 * b))));
-
-                label1.Text = "ln((sin a^2 +cos b)/(√1+(e/((a^3) + 3,4*b)))) = " + expResult.ToString();
-            }
+            InitializeComponent();
         }
-    
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double result = Math.Log((Math.Sin(Math.Pow(a, 2)) + Math.Cos(b)) / Math.Sqrt(1 + (Math.E / ((Math.Pow(a, 3)) + 3.4 * b))));
 
+            textBox4.Text = "Результат: " + result.ToString();
+        }
+
+        
+    }
 }
