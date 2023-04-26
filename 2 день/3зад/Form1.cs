b0eafec73ce16a787aeb44d95b2286132bff2efd
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-26 10:56:46 +0300

    ..

--- 2 день/3зад/Form1.cs
+++ 2 день/3зад/Form1.cs
@@ -18,45 +18,49 @@ namespace _3зад
                 InitializeComponent();
             }
 
-            private void button1_Click(object sender, EventArgs e)
+            
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double x1 = 0.1;
+            double x2 = 0.77;
+            double x3 = -0.9;
+            double a = 0.001;
+            double sum1 = 1;
+            double sum2 = 1;
+            double sum3 = 1;
+            int n = 1;
+
+            
+            // Вычисление для x1
+            while (Math.Abs(sum1) >= a)
             {
-                double x1 = 0.1;
-                double x2 = 0.77;
-                double x3 = -0.9;
-                double a = 0.001;
-                double sum1 = 1;
-                double sum2 = 1;
-                double sum3 = 1;
-                int n = 1;
-
-                // Вычисление для x1
-                while (Math.Abs(sum1) >= a)
-                {
-                    double term = Math.Pow(-1, n - 1) * n * Math.Pow(x1, n - 1);
-                    sum1 += term;
-                    n++;
-                }
-                textBox1.Text += "F(" + x1 + ") = " + sum1.ToString() + Environment.NewLine;
-
-                // Вычисление для x2
-                n = 1;
-                while (Math.Abs(sum2) >= a)
-                {
-                    double term = Math.Pow(-1, n - 1) * n * Math.Pow(x2, n - 1);
-                    sum2 += term;
-                    n++;
-                }
-                textBox2.Text += "F(" + x2 + ") = " + sum2.ToString() + Environment.NewLine;
-
-                // Вычисление для x3
-                n = 1;
-                while (Math.Abs(sum3) >= a)
-                {
-                    double term = Math.Pow(-1, n - 1) * n * Math.Pow(x3, n - 1);
-                    sum3 += term;
-                    n++;
-                }
-                textBox3.Text += "F(" + x3 + ") = " + sum3.ToString() + Environment.NewLine;
+                double term = Math.Pow(-1, n - 1) * n * Math.Pow(x1, n - 1);
+                sum1 += term;
+                n++;
             }
+            textBox1.Text += "F(" + x1 + ") = " + sum1.ToString() + Environment.NewLine;
+
+            // Вычисление для x2
+            n = 1;
+            while (Math.Abs(sum2) >= a)
+            {
+                double term = Math.Pow(-1, n - 1) * n * Math.Pow(x2, n - 1);
+                sum2 += term;
+                n++;
+            }
+            textBox2.Text += "F(" + x2 + ") = " + sum2.ToString() + Environment.NewLine;
+
+            // Вычисление для x3
+            n = 1;
+            while (Math.Abs(sum3) >= a)
+            {
+                double term = Math.Pow(-1, n - 1) * n * Math.Pow(x3, n - 1);
+                sum3 += term;
+                n++;
+            }
+           
+            textBox3.Text += "F(" + x3 + ") = " + sum3.ToString() + Environment.NewLine;
         }
+    }
 }
