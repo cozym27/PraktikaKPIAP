0067665488b719c89b3d5950c8f22a862227b2f4
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-27 12:23:01 +0300

    ..

--- 4 день/Form1.cs
+++ 4 день/Form1.cs
@@ -16,5 +16,16 @@ namespace _4_день
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(textBox1.Text); // считываем значение N из текстового поля
+            int[] arr = new int[N]; // создаем массив размера N
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = (int)Math.Pow(2, i + 1); // заполняем массив степенями двойки от первой до N-й
+            }
+            // выводим массив в текстовое поле
+            textBox2.Text = string.Join(", ", arr);
+        }
     }
 }
