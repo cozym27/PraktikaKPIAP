0e03fa861511382389df460ec067a3466a8c6652
alexandr.mokhor@mail.ru <alexandr.mokhor@mail.ru>
2023-04-27 08:34:11 +0300

    //

--- 3 день/2зад/Form1.Designer.cs
+++ 3 день/2зад/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.textBoxY = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,7 +46,7 @@
             // 
             // textBoxY
             // 
-            this.textBoxY.Location = new System.Drawing.Point(302, 144);
+            this.textBoxY.Location = new System.Drawing.Point(301, 147);
             this.textBoxY.Name = "textBoxY";
             this.textBoxY.Size = new System.Drawing.Size(127, 20);
             this.textBoxY.TabIndex = 1;
@@ -67,11 +69,31 @@
             this.pictureBoxCanvas.TabIndex = 3;
             this.pictureBoxCanvas.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите значение x:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите значение y:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBoxCanvas);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxY);
@@ -90,6 +112,8 @@
         private System.Windows.Forms.TextBox textBoxY;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBoxCanvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
